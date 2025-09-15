388e88f1c8904858cc1d60f87b16c49f15256c0e
Harry Nolan-Holmes <harry@nolanholmes.com>
2025-09-15 11:58:29 +1000

    Implement council user creation form with validation and role restrictions

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Model
 
 # Create your models here.
+
+# Remove redundant UserProfile and Council models - these are already defined in ricd app
