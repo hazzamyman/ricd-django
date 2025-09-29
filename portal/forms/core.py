9b5169952dec3cb7fd5dec1086904818d172663c
Harry <harry@debian-dev.local>
2025-09-30 08:37:44 +1000

    AUTO: Add SiteConfigurationForm to fix ImportError

--- portal/forms/core.py
+++ portal/forms/core.py
@@ -1,5 +1,5 @@
 from django import forms
-from ricd.models import Council, Program, Officer
+from ricd.models import Council, Program, Officer, SiteConfiguration
 
 
 class CouncilForm(forms.ModelForm):
@@ -113,4 +113,32 @@ class ProgramForm(forms.ModelForm):
             'funding_source': forms.Select(attrs={
                 'class': 'form-select'
             }),
+        }
+
+# Site Configuration Form
+class SiteConfigurationForm(forms.ModelForm):
+    """Form for site-wide configuration settings"""
+
+    class Meta:
+        model = SiteConfiguration
+        fields = '__all__'
+        widgets = {
+            'date_format': forms.Select(attrs={'class': 'form-select'}),
+            'time_format': forms.Select(attrs={'class': 'form-select'}),
+            'timezone': forms.Select(attrs={'class': 'form-select'}),
+            'default_currency': forms.Select(attrs={'class': 'form-select'}),
+            'currency_symbol': forms.TextInput(attrs={'class': 'form-control'}),
+            'currency_position': forms.Select(attrs={'class': 'form-select'}),
+            'default_language': forms.Select(attrs={'class': 'form-select'}),
+            'decimal_places': forms.NumberInput(attrs={'class': 'form-control'}),
+            'thousands_separator': forms.TextInput(attrs={'class': 'form-control'}),
+            'decimal_separator': forms.TextInput(attrs={'class': 'form-control'}),
+            'enable_dark_mode': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'enable_animations': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'maintenance_mode': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'maintenance_message': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'site_title': forms.TextInput(attrs={'class': 'form-control'}),
+            'site_description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'support_email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'support_phone': forms.TextInput(attrs={'class': 'form-control'}),
         }
